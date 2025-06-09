b8bff385ae752fdbd4c7a47702ed3adfd40f0f45
Luis David <lrambao5432@gmail.com>
2025-06-09 17:07:39 -0500

    first version landing page

---
+++ src/utils/constanst.js
@@ -0,0 +1,24 @@
+export const SITE_TITLE = "Corporacion CLEV";
+
+export const SOCIAL_LINKS = [
+  {
+    name: "Facebook",
+    url: "https://www.facebook.com/corporacionclev",
+  },
+  {
+    name: "Instagram",
+    url: "https://www.instagram.com/corporacionclev",
+  },
+  {
+    name: "Twitter",
+    url: "https://www.twitter.com/corporacionclev",
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/company/corporacionclev",
+  },
+  {
+    name: "TikTok",
+    url: "https://www.tiktok.com/@corporacionclev",
+  },
+];
